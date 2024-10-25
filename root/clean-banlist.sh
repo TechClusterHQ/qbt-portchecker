2256400788eae9400c7024b2a29ad8c7b9046da6
TechClusterHQ <136372560+TechClusterHQ@users.noreply.github.com>
2024-10-25 13:36:58 +0200

    feat: add clean-banlist script

---
+++ root/clean-banlist.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bash
+
+log() {
+    local message="$1"
+    local timestamp=$(date +"%Y.%m.%dT%H:%M:%S")
+    echo "${timestamp} (qbt-portchecker) (cleaner) ${message}"
+}
+
+response=$(curl --silent --request POST --url "http://localhost:${WEBUI_PORT}/api/v2/app/setPreferences" --data "json={\"banned_IPs\": \"\"}")
+
+if [[ $? -eq 0 ]]; then
+    log "banlist cleared successfully"
+else
+    log "error clearing banlist"
